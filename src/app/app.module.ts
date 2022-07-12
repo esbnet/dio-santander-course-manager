b447f5b192f6a8cebf25a95b4d85400f02fc1769
Edmilson Soares <esbnet@gmail.com>
2022-07-12 16:42:34 -0300

    feat: :lipstick: add star component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CourseListComponent, StarComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
