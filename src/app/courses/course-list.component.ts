097c51b24d314f77cdf11886f6ebc2cb0d47a6ab
Edmilson Soares <esbnet@gmail.com>
2022-07-13 18:54:40 -0300

    feat: :sparkles:

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from './cource.service';
 import { Course } from './course';
 
 @Component({
@@ -7,29 +8,8 @@ import { Course } from './course';
 })
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
-
+  constructor(private courseService: CourseService) {}
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Course 1',
-        imageUrl: './assets/images/http.png',
-        price: 145.99,
-        code: '321',
-        duration: 180,
-        rating: 5,
-        releaseDate: '25/03/2022',
-      },
-      {
-        id: 1,
-        name: 'Course 2',
-        imageUrl: './assets/images/cli.png',
-        price: 99.99,
-        code: '123',
-        duration: 120,
-        rating: 3.7,
-        releaseDate: '25/06/2022',
-      },
-    ];
+    this.courses = this.courseService.retriveAll();
   }
 }
